b2876e9f8b5cb7c305922d4977b57c1ccebd7627
Mohmmadabujazar <mohmmadabujazar@yahoo.com>
2021-04-12 22:15:53 +0300

    classtybes folder contain 4 fails css js img index.html I did lab 02 in these files

---
+++ classtybes/js/aps.js
@@ -0,0 +1,85 @@
+/* eslint-disable no-undef */
+'use strict';
+
+let user = prompt('Are you ready to start a game');
+console.log(user);
+// user = user.tolowercase();
+switch(user){
+case 'yes':
+  alert('great countinu writ yes for countinue with us');
+  break;
+case 'no':
+  alert('you must wrrite yes to countinue the game');
+  break;
+default:
+  alert('please write \n yes or no')
+}
+let userb = prompt('Its simple questions about yourself Are you ready to answer');
+console.log(userb)
+// userb = userb.tolowercase();
+switch(userb){
+case 'yes':
+  alert('great countinu writ yes for countinue with us'+userb);
+  break;
+case 'no':
+  alert('you must wrrite yes to countinue the game');
+  break;
+default:
+  alert('please write \n yes or no')
+}
+let userd = prompt('are you excited ?');
+console.log(userd)
+// userd = userd.tolowercase();
+switch(userd){
+case 'yes':
+  alert('great countinu writ yes for countinue with us'+userd);
+  break;
+case 'no':
+  alert('you must wrrite yes to countinue the game');
+  break;
+default:
+  alert('please write \n yes or no')
+}
+let usere = prompt('are you agree ?');
+console.log(usere)
+// usere = usere.tolowercase();
+switch(usere){
+case 'yes':
+  alert('great countinu writ yes for countinue with us');
+  break;
+case 'no':
+  alert('you must wrrite yes to countinue the game');
+  break;
+default:
+  alert('please write \n yes or no')
+}
+let userf = prompt('lets go');
+console.log(userf)
+// userf = userf.tolowercase();
+switch(userf){
+case 'yes':
+  alert('we will strat right now');
+  break;
+case 'no':
+  alert('you must wrrite yes to countinue the game');
+  break;
+default:
+  alert('please write \n yes or no')
+}
+
+let kola1 =prompt('what is your name');
+function pepsi1(name){
+  console.log(name);
+  alert('nice to meet you Mr '+ name);
+
+}
+pepsi1(kola1);
+
+let kola2 =prompt('How old are you Mr '+kola1);
+function pepsi2(name){
+  console.log(name);
+  alert('I wish you health and wellness ');
+
+}
+pepsi2(kola2);
+
