c1ab66a339b22b71c075277d5fa63fdbefa38ac3
Mohmmadabujazar <mohmmadabujazar@yahoo.com>
2021-04-16 20:43:12 +0300

    final lap about Me use 3 files : index aps style

--- classtybes/js/aps.js
+++ classtybes/js/aps.js
@@ -1,72 +1,5 @@
-/* eslint-disable no-undef */
 'use strict';
-/*
-let user = prompt('Are you ready to start a game');
-console.log(user);
-// user = user.tolowercase();
-switch(user){
-case 'yes':
-  alert('great countinu writ yes for countinue with us');
-  break;
-case 'no':
-  alert('you must wrrite yes to countinue the game');
-  break;
-default:
-  alert('please write \n yes or no')
-}
-let userb = prompt('Its simple questions about yourself Are you ready to answer');
-console.log(userb)
-// userb = userb.tolowercase();
-switch(userb){
-case 'yes':
-  alert('great countinu writ yes for countinue with us'+userb);
-  break;
-case 'no':
-  alert('you must wrrite yes to countinue the game');
-  break;
-default:
-  alert('please write \n yes or no')
-}
-let userd = prompt('are you excited ?');
-console.log(userd)
-// userd = userd.tolowercase();
-switch(userd){
-case 'yes':
-  alert('great countinu writ yes for countinue with us'+userd);
-  break;
-case 'no':
-  alert('you must wrrite yes to countinue the game');
-  break;
-default:
-  alert('please write \n yes or no')
-}
-let usere = prompt('are you agree ?');
-console.log(usere)
-// usere = usere.tolowercase();
-switch(usere){
-case 'yes':
-  alert('great countinu writ yes for countinue with us');
-  break;
-case 'no':
-  alert('you must wrrite yes to countinue the game');
-  break;
-default:
-  alert('please write \n yes or no')
-}
-let userf = prompt('lets go');
-console.log(userf)
-// userf = userf.tolowercase();
-switch(userf){
-case 'yes':
-  alert('we will strat right now');
-  break;
-case 'no':
-  alert('you must wrrite yes to countinue the game');
-  break;
-default:
-  alert('please write \n yes or no')
-}
-*/
+
 let kola1 = prompt('what is your name');
 function pepsi1(name) {
   console.log(name);
@@ -117,94 +50,27 @@ while (qus4 !== 'yes' && qus4 !== 'no' && qus4 !== 'y' && qus4 !== 'n') {
 }
 console.log(qus4);
 alert('its my city , I live there >> OK');
-/*
-let qus5 = prompt('Where is Amman located?','Jordan or Saudi Arabia or UAE or Qatar').toLocaleLowerCase();
-while (qus5 !== 'Jordan' && qus5 !== 'Jo' && qus5 !== 'Jord' && qus5 !== 'jordan ' && qus5 !== 'Jor' && qus5 !== 'Jorda'){
-if(ques5 === 'Saudi Arabia' || ques5 === 'Saudi' || ques5 === 'Saudi ' || ques5 === ' Saudi Arabia'){
-  console.log(ques5)
-alert('Close answer - 5/10 , but try again')
-}else if(ques5 === 'UAE' || ques5 === ' UAE' || ques5 === 'Qatar' || ques5 === ' Qatar'){
-alert('your answer - 2/10 , try again')
-}
-qus5 = prompt('Please choose one of the following answers!', 'Jordan or Saudi Arabia or UAE or Qatar').toLocaleLowerCase();
-}
-  */
 
-
-// let qus6 = prompt('what the result : 5*5= ');
-// while(qus6 != 25){
-//  qus6 = prompt('Try again : 5*5= ');
-// }
-// console.log(qus6)
-// alert('right answer keep countinue')
-
-// let qus7 = prompt('what the result : 6*5= ');
-// while(qus6 != 30){
-//  qus7 = prompt('Try again : 6*5= ');
-// }
-// console.log(qus7)
-// alert('right answer keep countinue')
-
-
-// let qus8 = prompt('what the result : 7*5= ', '35 25 45 34');
-// if (qus8 == '25') {
-//   qus8 = prompt('Too low : 7*5= ', '35 25 45 34');
-// } else if (qus8 == '45') {
-//   qus8 = prompt('Too low : 7*5= ', '35 25 45 34');
-// } else if (qus8 == '34') {
-//   qus8 = prompt('Too high : 7*5= ', '35 25 45 34');
-// } else {
-//   while (qus8 !== '35') {
-//   qus8 = prompt('what the result : 7*5= ', '35 25 45 34');
-// //   }
-// // }
-// // if (qus8 =='35') {
-// //   alert('right answer');
-// // } else if (qus8 =='25'){
-// // alert('Too low');
-// // } else if (qus8 =='45'){
-// // alert('Too low');
-// // } else if (qus8 =='34'){
-// //   alert('Too high');
-// }
-
-// let qus8 = prompt('what the result : 7*5= ', '35 25 45 34');
-// while (qus8 !== '35') {
-//   qus8 = prompt('what the result : 7*5= ', '35 25 45 34');
-//   if (qus8 === '25') {
-//     qus8 = prompt('Too low but try Again : 7*5= ', '35 25 45 34');
-//   }
-//   if (qus8 === '45') {
-//     qus8 = prompt('Too low but try Again : 7*5= ', '35 25 45 34');
-//   }
-//   if (qus8 === '34') {
-//     qus8 = prompt('Too high but try Again : 7*5= ', '35 25 45 34');
-//   }
-// }
-
-
-let i = 0
+let i = 0;
 let qus8 = prompt('what the result for: 7*5= have 4 attempt to get answer', '35 25 45 34');
 for (i = 0; i < 3; i++) {
-  if (qus8 == '25') {
+  if (qus8 === '25') {
     qus8 = prompt('Too low but try Again : 7*5= ', '35 25 45 34');
   }
-  if (qus8 == '45') {
+  if (qus8 === '45') {
     qus8 = prompt('Too low but try Again : 7*5= ', '35 25 45 34');
   }
-  if (qus8 == '34') {
+  if (qus8 === '34') {
     qus8 = prompt('Too high but try Again : 7*5= ', '35 25 45 34');
-  } 
+  }
 }
-if (qus8 == '35') {
+if (qus8 === '35') {
   alert('Raight Answer');
 } else {
   while (qus8 !== '35' && qus8 !== '45' && qus8 !== '34' && qus8 !== '25' ){
-  qus8 = prompt('please choose one option fom there what the result for: 7*5=', '35 25 45 34');
+    qus8 = prompt('please choose one option fom there what the result for: 7*5=', '35 25 45 34');
+  }
 }
-}
-
-// let qus9 = ['Mohammad', 'Moahmoud','Ali','Khalid','Yaser']
 let qus9 = prompt('what the Name It starts with the letter: M have 6 attempt to get answer', 'Mohammad Moahmoud Ali Khalid Yaser');
 for (i = 0; i < 5; i++) {
   if (qus9 === 'Ali') {
@@ -225,7 +91,5 @@ if (qus9 === 'Moahmoud') {
 } else {
   while (qus9 !== 'Mohammad' && qus9 !== 'Moahmoud'){
     qus9 = prompt('please choose one option fom there , what the Name It starts with the letter:M', 'Mohammad Moahmoud Ali Khalid Yaser');
+  }
 }
-}
-
-
--- classtybes/js/classes02.js
+++ classtybes/js/classes02.js
@@ -1,23 +1,23 @@
-'use strict';
+// 'use strict';
 
-let kola1 =prompt('what is your name');
-function pepsi1(name){
-  console.log(name);
-  alert('nice to meet you Mr '+ name);
-}
-pepsi1(kola1);
+// let kola1 =prompt('what is your name');
+// function pepsi1(name){
+//   console.log(name);
+//   alert('nice to meet you Mr '+ name);
+// }
+// pepsi1(kola1);
 
-let kola2 =prompt('How old are you Mr '+kola1);
-function pepsi2(name){
-  console.log(name);
-  alert('I wish you health and wellness ');
-}
-pepsi2(kola2);
+// let kola2 =prompt('How old are you Mr '+kola1);
+// function pepsi2(name){
+//   console.log(name);
+//   alert('I wish you health and wellness ');
+// }
+// pepsi2(kola2);
 
-let qus = prompt('Are you ready to start a game', 'Yes or No').toLocaleLowerCase();
-while(qus !== 'yes' && qus !== 'no' && qus !== 'y' && qus !== 'n'){
-    qus = prompt('Please enter Yes or No', 'yes or no').toLocaleLowerCase();
-}
+// let qus = prompt('Are you ready to start a game', 'Yes or No').toLocaleLowerCase();
+// while(qus !== 'yes' && qus !== 'no' && qus !== 'y' && qus !== 'n'){
+//     qus = prompt('Please enter Yes or No', 'yes or no').toLocaleLowerCase();
+// }
 
 /*
 let user = prompt('Are you ready to start a game');
@@ -102,3 +102,201 @@ function pepsi2(name){
 }
 pepsi2(kola2);
 */
+/* eslint-disable no-undef */
+'use strict';
+/*
+let user = prompt('Are you ready to start a game');
+console.log(user);
+// user = user.tolowercase();
+switch(user){
+case 'yes':
+  alert('great countinu writ yes for countinue with us');
+  break;
+case 'no':
+  alert('you must wrrite yes to countinue the game');
+  break;
+default:
+  alert('please write \n yes or no')
+}
+let userb = prompt('Its simple questions about yourself Are you ready to answer');
+console.log(userb)
+// userb = userb.tolowercase();
+switch(userb){
+case 'yes':
+  alert('great countinu writ yes for countinue with us'+userb);
+  break;
+case 'no':
+  alert('you must wrrite yes to countinue the game');
+  break;
+default:
+  alert('please write \n yes or no')
+}
+let userd = prompt('are you excited ?');
+console.log(userd)
+// userd = userd.tolowercase();
+switch(userd){
+case 'yes':
+  alert('great countinu writ yes for countinue with us'+userd);
+  break;
+case 'no':
+  alert('you must wrrite yes to countinue the game');
+  break;
+default:
+  alert('please write \n yes or no')
+}
+let usere = prompt('are you agree ?');
+console.log(usere)
+// usere = usere.tolowercase();
+switch(usere){
+case 'yes':
+  alert('great countinu writ yes for countinue with us');
+  break;
+case 'no':
+  alert('you must wrrite yes to countinue the game');
+  break;
+default:
+  alert('please write \n yes or no')
+}
+let userf = prompt('lets go');
+console.log(userf)
+// userf = userf.tolowercase();
+switch(userf){
+case 'yes':
+  alert('we will strat right now');
+  break;
+case 'no':
+  alert('you must wrrite yes to countinue the game');
+  break;
+default:
+  alert('please write \n yes or no')
+}
+*/
+/*
+let qus5 = prompt('Where is Amman located?','Jordan or Saudi Arabia or UAE or Qatar').toLocaleLowerCase();
+while (qus5 !== 'Jordan' && qus5 !== 'Jo' && qus5 !== 'Jord' && qus5 !== 'jordan ' && qus5 !== 'Jor' && qus5 !== 'Jorda'){
+if(ques5 === 'Saudi Arabia' || ques5 === 'Saudi' || ques5 === 'Saudi ' || ques5 === ' Saudi Arabia'){
+  console.log(ques5)
+alert('Close answer - 5/10 , but try again')
+}else if(ques5 === 'UAE' || ques5 === ' UAE' || ques5 === 'Qatar' || ques5 === ' Qatar'){
+alert('your answer - 2/10 , try again')
+}
+qus5 = prompt('Please choose one of the following answers!', 'Jordan or Saudi Arabia or UAE or Qatar').toLocaleLowerCase();
+}
+  */
+
+
+// let qus6 = prompt('what the result : 5*5= ');
+// while(qus6 != 25){
+//  qus6 = prompt('Try again : 5*5= ');
+// }
+// console.log(qus6)
+// alert('right answer keep countinue')
+
+// let qus7 = prompt('what the result : 6*5= ');
+// while(qus6 != 30){
+//  qus7 = prompt('Try again : 6*5= ');
+// }
+// console.log(qus7)
+// alert('right answer keep countinue')
+
+
+// let qus8 = prompt('what the result : 7*5= ', '35 25 45 34');
+// if (qus8 == '25') {
+//   qus8 = prompt('Too low : 7*5= ', '35 25 45 34');
+// } else if (qus8 == '45') {
+//   qus8 = prompt('Too low : 7*5= ', '35 25 45 34');
+// } else if (qus8 == '34') {
+//   qus8 = prompt('Too high : 7*5= ', '35 25 45 34');
+// } else {
+//   while (qus8 !== '35') {
+//   qus8 = prompt('what the result : 7*5= ', '35 25 45 34');
+// //   }
+// // }
+// // if (qus8 =='35') {
+// //   alert('right answer');
+// // } else if (qus8 =='25'){
+// // alert('Too low');
+// // } else if (qus8 =='45'){
+// // alert('Too low');
+// // } else if (qus8 =='34'){
+// //   alert('Too high');
+// }
+
+// let qus8 = prompt('what the result : 7*5= ', '35 25 45 34');
+// while (qus8 !== '35') {
+//   qus8 = prompt('what the result : 7*5= ', '35 25 45 34');
+//   if (qus8 === '25') {
+//     qus8 = prompt('Too low but try Again : 7*5= ', '35 25 45 34');
+//   }
+//   if (qus8 === '45') {
+//     qus8 = prompt('Too low but try Again : 7*5= ', '35 25 45 34');
+//   }
+//   if (qus8 === '34') {
+//     qus8 = prompt('Too high but try Again : 7*5= ', '35 25 45 34');
+//   }
+// }
+/*
+let qus5 = prompt('Where is Amman located?','Jordan or Saudi Arabia or UAE or Qatar').toLocaleLowerCase();
+while (qus5 !== 'Jordan' && qus5 !== 'Jo' && qus5 !== 'Jord' && qus5 !== 'jordan ' && qus5 !== 'Jor' && qus5 !== 'Jorda'){
+if(ques5 === 'Saudi Arabia' || ques5 === 'Saudi' || ques5 === 'Saudi ' || ques5 === ' Saudi Arabia'){
+  console.log(ques5)
+alert('Close answer - 5/10 , but try again')
+}else if(ques5 === 'UAE' || ques5 === ' UAE' || ques5 === 'Qatar' || ques5 === ' Qatar'){
+alert('your answer - 2/10 , try again')
+}
+qus5 = prompt('Please choose one of the following answers!', 'Jordan or Saudi Arabia or UAE or Qatar').toLocaleLowerCase();
+}
+  */
+
+
+// let qus6 = prompt('what the result : 5*5= ');
+// while(qus6 != 25){
+//  qus6 = prompt('Try again : 5*5= ');
+// }
+// console.log(qus6)
+// alert('right answer keep countinue')
+
+// let qus7 = prompt('what the result : 6*5= ');
+// while(qus6 != 30){
+//  qus7 = prompt('Try again : 6*5= ');
+// }
+// console.log(qus7)
+// alert('right answer keep countinue')
+
+
+// let qus8 = prompt('what the result : 7*5= ', '35 25 45 34');
+// if (qus8 == '25') {
+//   qus8 = prompt('Too low : 7*5= ', '35 25 45 34');
+// } else if (qus8 == '45') {
+//   qus8 = prompt('Too low : 7*5= ', '35 25 45 34');
+// } else if (qus8 == '34') {
+//   qus8 = prompt('Too high : 7*5= ', '35 25 45 34');
+// } else {
+//   while (qus8 !== '35') {
+//   qus8 = prompt('what the result : 7*5= ', '35 25 45 34');
+// //   }
+// // }
+// // if (qus8 =='35') {
+// //   alert('right answer');
+// // } else if (qus8 =='25'){
+// // alert('Too low');
+// // } else if (qus8 =='45'){
+// // alert('Too low');
+// // } else if (qus8 =='34'){
+// //   alert('Too high');
+// }
+
+// let qus8 = prompt('what the result : 7*5= ', '35 25 45 34');
+// while (qus8 !== '35') {
+//   qus8 = prompt('what the result : 7*5= ', '35 25 45 34');
+//   if (qus8 === '25') {
+//     qus8 = prompt('Too low but try Again : 7*5= ', '35 25 45 34');
+//   }
+//   if (qus8 === '45') {
+//     qus8 = prompt('Too low but try Again : 7*5= ', '35 25 45 34');
+//   }
+//   if (qus8 === '34') {
+//     qus8 = prompt('Too high but try Again : 7*5= ', '35 25 45 34');
+//   }
+// }
+
